d16943a51cf2a08cb3e0e1fa4b4e8ebb9bd596f3
Sushrut Ghimire <sushrutghimire1@gmail.com>
2021-02-25 16:03:27 +0545

    removed auth for create client

--- src/main/java/com/vit/startupapp/helperutils/CorsConfig.java
+++ src/main/java/com/vit/startupapp/helperutils/CorsConfig.java
@@ -4,17 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig {
-
+class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
             }
         };
     }
